d53cb524e860225d3cfb0b6d519aef481f60960f
careertraining1357 <139525570+careertraining1357@users.noreply.github.com>
2023-07-14 10:31:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Bluberry Oat Meal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
